05fed78cf8f5ba57a8b74772a4d05a1f5ef0a2cf
Aanchal Saxena <aanchalsaxenacontact@gmail.com>
2024-07-06 22:36:39 +0530

    tabbar

---
+++ DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  Shelves-User
+//
+//  Created by Sahil Raj on 06/07/24.
+//
+
+import Foundation
---
+++ Tabbar/Tab.swift
@@ -0,0 +1,36 @@
+//
+//  Tab.swift
+//  Shelves-User
+//
+//  Created by Sahil Raj on 06/07/24.
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable
+{
+    case forYou = "For You"
+    case explore = "Explore"
+    case myLibrary = "My Library"
+    case profile = "Profile"
+    
+    var systemImage: String
+    {
+        switch self{
+        case .explore:
+            return "magnifyingglass"
+            
+        case .myLibrary:
+            return "book"
+            
+        case .forYou:
+            return "house"
+            
+        case .profile:
+            return "person"
+        }
+    }
+    var index: Int{
+        return Tab.allCases.firstIndex(of: self) ?? 0
+    }
+}
